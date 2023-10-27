5bd4655a5dffe1120fae351298dc910a5d0f07ab
cbt@thermosphr.com <cbt@thermosphr.com>
2023-10-27 16:58:42 +0500

    addedaddition folder

---
+++ Addition/add.py
@@ -0,0 +1,10 @@
+def divide(numerator: float, denominator: float) -> float:
+    ans = numerator/denominator
+    return ans
+
+
+def add(num1: float, num2: float) -> float:
+    ans = num1 + num2
+    return ans
+
+
